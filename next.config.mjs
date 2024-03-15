c323e6180242dbc88b93c6cbda4d39eb48a74502
samgende <sam.gend1415@gmail.com>
2024-03-15 18:56:53 +0100

    fix: next configs compiles

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,19 @@
 import rehypePrism from '@mapbox/rehype-prism'
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
+import withMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
-
+/*
 const withMDX = nextMDX('@next/mdx')({
   extension: /\.mdx$/,
   options: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism],
   },
-})
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  module: {
-    rules: [
-      {
-        test: /\.mdx$/,
-        use: ['babel-loader', '@mdx-js/loader'],
-      },
-    ],
-  },
-})
+})*/
+
 export default withMDX(nextConfig)
