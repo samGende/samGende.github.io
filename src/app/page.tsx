521ccc00eb7d1424337ee94776f6d33f9417b2b3
samgende <sam.gend1415@gmail.com>
2024-03-15 19:43:57 +0100

    fix: change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -281,7 +281,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            International Student, Research Assistant, interested in building cool Sh-t
+            International Student, Research Assistant, interested in building smart stuff
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I'm Sam Gende a soon to be graduate student, that is looking to contiunue to
