afd7a306763abfbb79409a336cea22d3088f13a9
samgende <sam.gend1415@gmail.com>
2024-03-15 18:27:40 +0100

    fix: config tests

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const withMDX = nextMDX('@next/mdx')({
     rehypePlugins: [rehypePrism],
   },
 })
-
-module.exports = withMDX()
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+})
 export default withMDX(nextConfig)
