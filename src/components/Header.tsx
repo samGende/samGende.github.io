0413af567ebbc773d18f8e376be415caa342e9e0
samgende <sam.gende1415@gmail.com>
2025-02-06 20:20:21 -0600

    fix links in header and footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -179,7 +179,7 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about1">About</NavItem>
+        <NavItem href="/">About</NavItem>
         <NavItem href="/articles1">Articles</NavItem>
         <NavItem href="/projects1">Projects</NavItem>
         <NavItem href="/uses1">Uses</NavItem>
